2042ac1d04b26a44c5dda410645142b3b32a7097
deano <me@deanoc.com>
2018-11-29 23:25:14 +0200

    Simple pad controlled camera impl (WIP) in gfxtest

--- apps/gfxtest/gfxtest.cpp
+++ apps/gfxtest/gfxtest.cpp
@@ -25,6 +25,59 @@
 #include "input/keyboard.h"
 #include "timing/tickerclock.h"
 #include "meshops/shapes.h"
+#include "input/provider.h"
+#include "input/vpadlistener.h"
+
+struct SimplePadCamera : public Input::VPadListener
+{
+	SimplePadCamera()
+	{
+		simpleEye.setProjection(60.0f, 1280.0f / 720.0f, 0.01f);
+		lookatPoint = Math::vec3(0, 0, 0);
+		position = Math::vec3(0, 0, 4);
+		update(0.0);
+	}
+
+	~SimplePadCamera() final
+	{
+
+	}
+
+	void axisMovement(Input::VPadAxisId id_, float delta_) final
+	{
+		switch(id_)
+		{
+			case Input::VPadAxisId::LX:
+				lookatPoint.x += delta_ * 0.1f;
+				position.x += delta_ * 0.1f;
+				break;
+			case Input::VPadAxisId::LY:
+				lookatPoint.z += delta_ * 0.1f;
+				position.z += delta_ * 0.1f;
+				break;
+
+			default: break;
+
+		}
+	}
+
+	void button(Input::VPadButtonId id_, float delta_) final
+	{
+	}
+
+	auto update(double deltaT_) -> void
+	{
+		Math::mat4x4 view = Math::lookAt(position,
+										 lookatPoint,
+										 Math::vec3(0, 1, 0));
+		simpleEye.setView(view);
+	}
+
+	Math::vec3 position;
+	Math::vec3 lookatPoint;
+
+	MidRender::SimpleEye simpleEye;
+};
 
 struct App
 {
@@ -80,7 +133,7 @@ struct App
 		windowConfig.windowName = "Gfx Test"s;
 		windowConfig.fullscreen = false;
 		windowConfig.directInput = true;
-		auto window = shell.createPresentableWindow(windowConfig);
+		window = shell.createPresentableWindow(windowConfig);
 		DeviceConfig config = {
 				true,    // presentable
 				true,    // renderer
@@ -95,6 +148,7 @@ struct App
 
 		weakDisplay = device->getDisplay();
 
+		tickerClock = std::make_unique<Timing::TickerClock>();
 		createResources();
 
 		return okay;
@@ -108,6 +162,8 @@ struct App
 		meshModRenderer.reset();
 
 		resourceManager->flushCache();
+
+		tickerClock.reset();
 		weakDisplay.reset();
 		device.reset();
 		resourceManager.reset();
@@ -159,26 +215,28 @@ struct App
 		auto renderQueue = device->getGeneralQueue();
 		auto rEncoderPool = device->makeEncoderPool(true, CommandQueueFlavour::Render);
 
-		auto simpleEye = new SimpleEye();
-		simpleEye->setProjection(60.0f, 1280.0f / 720.0f, 0.01f);
-		Math::mat4x4 view = Math::lookAt(Math::vec3(0, 0, 4),
-										 Math::vec3(0, 0, 0),
-										 Math::vec3(0, 1, 0));
-		simpleEye->setView(view);
+		simplePadCamera = std::make_shared<SimplePadCamera>();
+		inputProvider = shell.getInputProvider(window);
+		inputProvider->setVirtualPadListener(0, simplePadCamera);
+		tickerClock->update();
 
 		float yrot = 0.0f;
 		using namespace Core::bitmask;
 		do
 		{
+			auto deltaT = tickerClock->update();
+			inputProvider->update(deltaT);
+			simplePadCamera->update(deltaT);
+
 			rEncoderPool->reset();
 
 			Math::mat4x4 rootMatrix = Math::rotate(Math::identity<Math::mat4x4>(), yrot, Math::vec3(0, 1, 0));
-			yrot += 0.001f;
 			while(yrot > Math::two_pi<float>())
 			{
 				yrot -= Math::two_pi<float>();
 			}
 
+			auto const simpleEye = &simplePadCamera->simpleEye;
 			SimpleForwardGlobals* globals = (SimpleForwardGlobals*) globalBuffer->map();
 			std::memcpy(globals->viewMatrix, &simpleEye->getView(), sizeof(float) * 16);
 			std::memcpy(globals->projectionMatrix, &simpleEye->getProjection(), sizeof(float) * 16);
@@ -230,6 +288,10 @@ struct App
 	Shell::ShellInterface& shell;
 	Render::Device::Ptr device;
 	Render::Display::WeakPtr weakDisplay;
+	Shell::PresentableWindow* window;
+	std::unique_ptr<Timing::TickerClock> tickerClock;
+	std::shared_ptr<SimplePadCamera> simplePadCamera;
+	std::unique_ptr<Input::Provider> inputProvider;
 
 	std::unique_ptr<MidRender::ImguiBindings> imguiBindings;
 	std::unique_ptr<MidRender::MeshModRenderer> meshModRenderer;
